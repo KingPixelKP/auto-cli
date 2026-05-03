8b1933c3df9fd6bf1ad86eb6a497f7dc306c5ed5
Gabriel-Matias <gabrielmf1977@gmail.com>
2026-05-03 18:35:01 +0100

    package

---
+++ packages/a/auto-cli/xmake.lua
@@ -0,0 +1,17 @@
+package("auto-cli")
+    set_description("The auto-cli package")
+
+    add_urls("https://github.com/KingPixelKP/auto-cli.git")
+    add_versions("0.1.0", "680278edd02466356d88e8d26fc43768a32379d8")
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        
+    end)
--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,21 @@
-add_rules("mode.debug", "mode.release")
-add_rules("plugin.compile_commands.autoupdate", {outputdir = "build"})
+set_project("auto-cli")
+set_version("0.1.0")
+set_languages("c++23")
 
-target("main")
-    set_kind("binary")
-    add_deps("auto-cli")
-    add_files("main.cpp")
+option("build_main")
+    set_default(true)
+    set_description("Build the example main")
+option_end()
+
+if has_config("build_main") then
+    add_rules("mode.debug", "mode.release")
+    add_rules("plugin.compile_commands.autoupdate", {outputdir = "build"})
+
+    target("main")
+        set_kind("binary")
+        add_deps("auto-cli")
+        add_files("main.cpp")
+end
 
 
 includes("auto-cli")
