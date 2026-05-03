07da926d11c6de1feaa9f7a615d819ec3a1c18ae
Gabriel-Matias <gabrielmf1977@gmail.com>
2026-05-03 20:20:22 +0100

    Fix

--- xmake.lua
+++ xmake.lua
@@ -2,21 +2,6 @@ set_project("auto-cli")
 set_version("0.1.0")
 set_languages("c++23")
 
-option("build_main")
-    set_default(true)
-    set_description("Build the example main")
-option_end()
-
-if has_config("build_main") then
-    add_rules("mode.debug", "mode.release")
-    add_rules("plugin.compile_commands.autoupdate", {outputdir = "build"})
-
-    target("main")
-        set_kind("binary")
-        add_deps("auto-cli")
-        add_files("main.cpp")
-end
-
 target("auto-cli")
     set_kind("static")
     set_languages("c++20", {public = true})
@@ -24,4 +9,4 @@ target("auto-cli")
     add_includedirs("include", {public = true})
     add_includedirs("include/auto-cli/detail", {private = true})
     add_includedirs("src", {private = true})
-    add_headerfiles("include/(auto-cli/*.h)", {public = true})
+    add_headerfiles("include/**.h", {public = true})
