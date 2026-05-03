5b6a7e23040fa97209013f987f065bd8bca0adf5
Gabriel-Matias <gabrielmf1977@gmail.com>
2026-05-03 18:39:37 +0100

    commit hash

--- packages/a/auto-cli/xmake.lua
+++ packages/a/auto-cli/xmake.lua
@@ -2,7 +2,7 @@ package("auto-cli")
     set_description("The auto-cli package")
 
     add_urls("https://github.com/KingPixelKP/auto-cli.git")
-    add_versions("0.1.0", "680278edd02466356d88e8d26fc43768a32379d8")
+    add_versions("0.1.0", "8b1933c3df9fd6bf1ad86eb6a497f7dc306c5ed5")
 
     on_install(function (package)
         local configs = {}
