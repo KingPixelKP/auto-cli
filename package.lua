680278edd02466356d88e8d26fc43768a32379d8
Gabriel-Matias <gabrielmf1977@gmail.com>
2026-05-03 18:19:23 +0100

    package

--- package.lua
+++ package.lua
@@ -3,7 +3,7 @@ package("auto-cli")
     set_description("A lightweight C++ CLI argument parser")
 
     add_urls("https://github.com/KingPixelKP/auto-cli/archive/refs/tags/$(version).tar.gz")
-    add_versions("0.1.0", "<sha256 of the tarball>")
+    add_versions("0.1.0", "914923a41d047df70520954bc02f958b225780503921ecaaa17b0eeb31960d13")
 
     on_install(function(package)
         import("package.tools.xmake").install(package)
